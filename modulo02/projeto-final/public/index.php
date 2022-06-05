29c64f3822b8243dfd69e94cb748fe59ba19f4d8
Igor Tozetti <igorct77@gmail.com>
2022-06-05 20:08:48 -0300

    rotas

--- modulo02/projeto-final/public/index.php
+++ modulo02/projeto-final/public/index.php
@@ -7,21 +7,32 @@ use App\Controller\ProductController;
 use App\Controller\CategoryController;
 use App\Controller\ErrorController;
 
-
 $url = explode("?",  $_SERVER['REQUEST_URI'])[0];
-if($url === '/') {
-    $c = new IndexController();
-    $c->indexAction();   
-} elseif($url === '/login') {
-    $c = new Indexcontroller();
-    $c->loginAction();
-}elseif($url === '/produtos') {
-    $c = new ProductController();
-    $c->listAction();
-} else {
-    $error = new ErrorController();
-    $error->notFoundAction();
+
+function createRoute(string $controllerName, string $methodName) {
+    return [
+        'controller' => $controllerName,
+        'method' => $methodName,
+    ];
 }
+$routes = [
+    '/' => createRoute(IndexController::class, 'indexAction'),
+    '/produtos' => createRoute(ProductController::class, 'listAction'),
+    '/categoria' => createRoute(CategoryController::class, 'listAction'),
+
+];
+
+if(false === isset($routes[$url])) {
+    // $e = new ErrorController();
+    // $e->notFoundAction();
+    ( new ErrorController() )->notFoundAction();
+    exit;
+}
+$controllerName = $routes[$url]['controller'];
+$methodName = $routes[$url]['method'];
+
+(new $controllerName())->$methodName();
+
 
 // $c = new IndexController();
 // // $c->indexAction();
--- modulo02/projeto-final/src/View/teste.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo '<h1>Olá, estou dentro de view!</h1>';
