b6b3a815a1bc3c207dcd11b3d1dcefa28801646e
Igor Tozetti <igorct77@gmail.com>
2022-06-05 19:38:13 -0300

    URLs

--- modulo02/projeto-final/public/index.php
+++ modulo02/projeto-final/public/index.php
@@ -5,7 +5,23 @@ include '../vendor/autoload.php';
 use App\Controller\IndexController;
 use App\Controller\ProductController;
 use App\Controller\CategoryController;
+use App\Controller\ErrorController;
 
+
+$url = explode("?",  $_SERVER['REQUEST_URI'])[0];
+if($url === '/') {
+    $c = new IndexController();
+    $c->indexAction();   
+} elseif($url === '/login') {
+    $c = new Indexcontroller();
+    $c->loginAction();
+}elseif($url === '/produtos') {
+    $c = new ProductController();
+    $c->listAction();
+} else {
+    $error = new ErrorController();
+    $error->notFoundAction();
+}
 
 // $c = new IndexController();
 // // $c->indexAction();
@@ -18,7 +34,6 @@ use App\Controller\CategoryController;
 // $p->editAction();
 
 
-$category = new CategoryController();
-$category->listAction();
-
+// $category = new CategoryController();
+// $category->listAction();
 
